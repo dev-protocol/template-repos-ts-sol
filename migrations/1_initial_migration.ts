83ace7953de7def2c3a0d8f78f6b62e37d4fee47
aggre <hiroyuki.aggre@gmail.com>
2020-01-22 12:29:41 +0900

    migrations using typescript

--- migrations/1_initial_migration.ts
+++ migrations/1_initial_migration.ts
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require('Migrations')
 
-module.exports = function(deployer) {
+const handler = function(deployer) {
 	deployer.deploy(Migrations)
-}
+} as Truffle.Migration
+
+export = handler
