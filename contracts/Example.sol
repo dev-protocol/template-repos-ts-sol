1ce3abc7810a9f68f6318fc5cb4ab81ac5e7a9ae
aggre <hiroyuki.aggre@gmail.com>
2021-09-18 00:51:55 +0900

    remove unnecessary library

--- contracts/Example.sol
+++ contracts/Example.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MPL-2.0
 pragma solidity =0.8.7;
 
-import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract Example is Initializable {
-	using SafeMath for uint256;
 	uint256 public value;
 
 	function initialize(uint256 initialValue) external initializer {
@@ -13,6 +11,6 @@ contract Example is Initializable {
 	}
 
 	function add(uint256 v) external {
-		value = value.add(v);
+		value = value + v;
 	}
 }
