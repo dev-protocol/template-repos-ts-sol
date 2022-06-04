8a019e5e9afa11dcbb27b1dda516d7632c8f218e
aggre <hiroyuki.aggre@gmail.com>
2022-06-04 16:30:03 +0900

    supports @nomiclabs/hardhat-etherscan

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
 # template-repos-ts-sol
 
-Template repository for using TypeScript and Solidity
-
-# Usage
-
-Create a repository using this template; just runs following command.
+# Installation
 
 ```bash
 yarn
+```
+
+# Verifying a deployed contracts
+
+## For a contract with no constructor arguments
+
+Run hardhat-etherscan
+
+```bash
+yarn hardhat verify --network <NETWORK_NAME> <CONTRACT_ADDRESS>
+```
+
+## For a contract with constructor arguments
+
+1. Create an arguments declaration file
+
+```js
+// cat ./scripts/arguments.js
+module.exports = [
+	'arguments 1',
+	'arguments 2',
+	'arguments 3',
+	...
+]
+```
+
+2. Run hardhat-etherscan
+
+```bash
+yarn hardhat verify --network <NETWORK_NAME> <CONTRACT_ADDRESS> --contract <PATH_TO_CONTRACT>:<CONTRACT_NAME> --constructor-args ./scripts/arguments.js
 ```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,66 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-etherscan'
+import { HardhatUserConfig } from 'hardhat/config'
+import * as dotenv from 'dotenv'
 
-module.exports = {
-	solidity: '0.8.7',
+dotenv.config()
+
+const mnemnoc =
+	typeof process.env.MNEMONIC === 'undefined' ? '' : process.env.MNEMONIC
+
+const config: HardhatUserConfig = {
+	solidity: {
+		version: '0.8.7',
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 1000,
+			},
+		},
+	},
+	networks: {
+		mainnet: {
+			url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY!}`,
+			accounts: {
+				mnemonic: mnemnoc,
+			},
+		},
+		arbitrumOne: {
+			url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_KEY!}`,
+			accounts: {
+				mnemonic: mnemnoc,
+			},
+		},
+		arbitrumRinkeby: {
+			url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_KEY!}`,
+			accounts: {
+				mnemonic: mnemnoc,
+			},
+		},
+		polygonMainnet: {
+			url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_KEY!}`,
+			accounts: {
+				mnemonic: mnemnoc,
+			},
+		},
+		polygonMumbai: {
+			url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY!}`,
+			accounts: {
+				mnemonic: mnemnoc,
+			},
+		},
+	},
+	etherscan: {
+		apiKey: {
+			mainnet: process.env.ETHERSCAN_KEY,
+			arbitrumOne: process.env.ARBISCAN_KEY,
+			arbitrumTestnet: process.env.ARBISCAN_RINKEBY_KEY,
+			polygon: process.env.POLYGONSCAN_KEY,
+			polygonMumbai: process.env.POLYGONSCAN_MUMBAI_KEY,
+		},
+	},
 }
+
+export default config
